9def0f42f9f7d9950f8440570fc194cfb51ac74f
mojimoon <cryovit@outlook.com>
2025-05-15 19:38:41 +0800

    chore(web): enhance styles

--- web/src/app/components/rank-list.tsx
+++ web/src/app/components/rank-list.tsx
@@ -5,24 +5,40 @@ import { MaterialReactTable, MRT_ColumnDef } from 'material-react-table';
 import { Box, Container, TextField, Typography } from '@mui/material';
 import { useEffect, useState } from 'react';
 
+const alignRight = {
+  align: 'right',
+};
+
 const columns: MRT_ColumnDef<FullOrder>[] = [
-  { accessorKey: 'idx', header: '序号' },
-  { accessorKey: 'vid', header: 'VNDB ID' },
-  { accessorKey: 'title_ja', header: '日文标题' },
-  { accessorKey: 'title_en', header: '英文标题' },
-  { accessorKey: 'title_zh', header: '中文标题' },
-  { accessorKey: 'alias', header: '别名' },
-  { accessorKey: 'total', header: '合计积分' },
-  { accessorKey: 'percentage', header: '比例积分' },
-  { accessorKey: 'simple', header: '简单积分' },
-  { accessorKey: 'weighted_simple', header: '加权简单' },
-  { accessorKey: 'pagerank', header: 'PageRank' },
-  { accessorKey: 'elo', header: 'Elo' },
-  // { accessorKey: 'appear', header: '出现次数' },
-  // { accessorKey: 'id', header: 'ID' },
-  { accessorKey: 'entropy', header: '熵值' },
-  { accessorKey: 'c_votecount', header: '投票数' },
-  { accessorKey: 'c_rating', header: '评分' },
+  { accessorKey: 'idx', header: '序号', maxSize: 80 },
+  { accessorKey: 'vid', header: 'VNDB ID', maxSize: 80,
+    Cell: ({ cell }) => {
+      const { row } = cell;
+      const { original } = row;
+      return (
+        <Typography className="text-blue-500">
+          <a href={`https://vndb.org/${original.vid}`} target="_blank" rel="noopener noreferrer">
+            {original.vid}
+          </a>
+        </Typography>
+      );
+    },
+  },
+  { accessorKey: 'title_ja', header: '日文标题', maxSize: 160 },
+  { accessorKey: 'title_en', header: '英文标题', maxSize: 160 },
+  { accessorKey: 'title_zh', header: '中文标题', maxSize: 160 },
+  { accessorKey: 'alias', header: '别名', maxSize: 160 },
+  { accessorKey: 'total', header: '合计积分', maxSize: 80, muiTableBodyCellProps: { align: 'right' } },
+  { accessorKey: 'percentage', header: '比例积分', maxSize: 80, muiTableBodyCellProps: { align: 'right' } },
+  { accessorKey: 'simple', header: '简单积分', maxSize: 80, muiTableBodyCellProps: { align: 'right' } },
+  { accessorKey: 'weighted_simple', header: '加权简单', maxSize: 80, muiTableBodyCellProps: { align: 'right' } },
+  { accessorKey: 'pagerank', header: 'PageRank', maxSize: 80, muiTableBodyCellProps: { align: 'right' } },
+  { accessorKey: 'elo', header: 'Elo', maxSize: 80, muiTableBodyCellProps: { align: 'right' } },
+  { accessorKey: 'entropy', header: '熵值', maxSize: 80, muiTableBodyCellProps: { align: 'right' } },
+  // { accessorKey: 'appear', header: '出现次数', maxSize: 80, muiTableBodyCellProps: { align: 'right' } },
+  // { accessorKey: 'id', header: 'ID', maxSize: 80, muiTableBodyCellProps: { align: 'right' } },
+  { accessorKey: 'c_votecount', header: '投票数', maxSize: 80, muiTableBodyCellProps: { align: 'right' } },
+  { accessorKey: 'c_rating', header: '评分', maxSize: 80, muiTableBodyCellProps: { align: 'right' } },
 ];
 
 export default function RankList() {
@@ -73,12 +89,14 @@ export default function RankList() {
         columns={columns}
         data={data}
         enableColumnActions={false}
+        sortDescFirst={false}
         state={{isLoading}}
         initialState={{
           columnVisibility: {
             idx: false,
             id: false,
             alias: false,
+            title_en: false,
             c_rating: false,
             c_votecount: false,
           },
