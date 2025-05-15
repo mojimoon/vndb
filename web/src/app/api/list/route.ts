002cc5ca0b7f1f1d6d9a24ea859477b70a75a650
mojimoon <cryovit@outlook.com>
2025-05-15 18:56:25 +0800

    feat(server): /api/list

--- web/src/app/api/list/route.ts
+++ web/src/app/api/list/route.ts
@@ -1,7 +1,37 @@
 // path: /api/list
 
 import { NextResponse } from 'next/server';
+import { FullOrder } from '@/app/common/struct';
+import path from 'path';
+import fs from 'fs';
+import Papa from 'papaparse';
+
+const fullOrderPath = path.join(process.cwd(), 'public', 'out', 'full_order.csv');
 
 export async function GET(request: Request) {
-  return NextResponse.json({ hello: 'world' });
+  
+  const csv = fs.readFileSync(fullOrderPath, 'utf-8');
+  const parsedData = Papa.parse<FullOrder>(csv, {
+    header: true,
+    skipEmptyLines: true,
+  });
+
+  const searchParams = new URL(request.url).searchParams;
+  const q = searchParams.get('q')?.trim().toLowerCase() || '';
+
+  let filtered = parsedData.data;
+
+  if (q) {
+    filtered = parsedData.data.filter((item) => {
+      return (
+        item.title_ja?.toLowerCase().includes(q) ||
+        item.title_en?.toLowerCase().includes(q) ||
+        item.title_zh?.toLowerCase().includes(q)
+      );
+    });
+  }
+
+  return NextResponse.json({
+    data: filtered,
+  });
 }
---
+++ web/src/app/common/struct.ts
@@ -0,0 +1,19 @@
+export type FullOrder = {
+  idx: string;
+  vid: string;
+  total: string;
+  percentage: string;
+  simple: string;
+  weighted_simple: string;
+  pagerank: string;
+  elo: string;
+  entropy: string;
+  appear: string;
+  id: string;
+  c_votecount: string;
+  c_rating: string;
+  alias?: string;
+  title_ja?: string;
+  title_en?: string;
+  title_zh?: string;
+};
