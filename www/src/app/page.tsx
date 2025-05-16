181075282b3ff2dee55fd15c0d0a6a54dc008b61
mojimoon <cryovit@outlook.com>
2025-05-16 16:55:18 +0800

    add theme switcher

--- www/src/app/page.tsx
+++ www/src/app/page.tsx
@@ -1,4 +1,4 @@
-export default function Home() {
+export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-4xl font-bold">Hello, world!</h1>
